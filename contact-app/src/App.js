aa786b0499cbfb7566f30e8078bdca2ea3b4a548
LeeSeunghyeon-1 <ujm0524@naver.com>
2022-01-02 02:34:08 +0900

    .

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
 
 class App extends Component {
+
+  id = 0;
+
+  state = {
+    information : [],
+  }
+
+  handleCreate = (date) => {
+    const {information} = this.state;
+    this.setState({
+      information : information.concat(Object.assign({}, data, {
+        id : this.id++
+      }))
+    });
+  }
+
   render() {
     return (
       <div>
-        <PhoneForm />
+        <PhoneForm onCreate={this.handleCreate}/>
+        {JSON.stringify(this.state.information)}
       </div>
     );
   }
+
 }
 
 export default App;
--- contact-app/src/components/PhoneForm.js
+++ contact-app/src/components/PhoneForm.js
@@ -12,14 +12,21 @@ class PhoneForm extends Component {
             [e.target.name] : e.target.value
         });
     }
+
+    handleSubmit = (e) => {
+        e.preventDefault(); //원래 해야하던 작업을 하지 않게 방지해주ㅁ
+        this.props.onCreate(this.state);// onCreate 함수 호출
+        this.setState({
+            name : '',
+            phone : '',
+        })
+    }
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}> {/*눌러도 새로고침이 되지 않도록*/}
                 <input name = 'name' placeholder='이름' onChange={this.handleChange} value={this.state.name}/>
                 <input name = 'phone' placeholder='전화번호' onChange={this.handleChange} value={this.state.phone}/>
-                <div>
-                    {this.state.name} {this.state.phone}
-                </div>
+                <button type = "submit">등록</button>
             </form>
         );
     }
