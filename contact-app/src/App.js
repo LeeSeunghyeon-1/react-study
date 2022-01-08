85e75ce96e0f78962bebdc37abe11bb6935595a7
LeeSeunghyeon-1 <ujm0524@naver.com>
2022-01-08 15:46:34 +0900

    .

--- contact-app/src/App.js
+++ contact-app/src/App.js
@@ -1,61 +1,39 @@
 import React, { Component } from 'react';
 import PhoneForm from './components/PhoneForm';
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
-
-  id = 0;
-
+  id = 2
   state = {
-    information : [],
+    information: [
+      {
+        id: 0,
+        name: '이승현',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
   }
-
-  handleCreate = (date) => {
-    const {information} = this.state;
+  handleCreate = (data) => {
+    const { information } = this.state;
     this.setState({
-      information : information.concat(Object.assign({}, data, {
-        id : this.id++
-      }))
-    });
+      information: information.concat({ id: this.id++, ...data })
+    })
   }
-
   render() {
     return (
       <div>
-        <PhoneForm onCreate={this.handleCreate}/>
-        {JSON.stringify(this.state.information)}
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+        <PhoneInfoList data={this.state.information}/>
       </div>
     );
   }
-
 }
 
 export default App;
-
-
-
-/*import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-*/
